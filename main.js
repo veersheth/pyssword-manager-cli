cbf166d17865f2d8270e35174765dbd8efc91bdf
Veer <vtube258@gmail.com>
2021-08-26 21:27:30 +0530

    Fixed error in JS File

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-//MAKING THE PASSWORD LISY
 const nums = ['1','2','3','4','5','6','7','8','9','0'];
 const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'];
 const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'];
@@ -13,8 +7,6 @@ const symbols = ['@','#','$','%','&','.','-','_'];
 
 const everyChar =  nums.concat(lowercase, uppercase, symbols);
 
-
-//MAKING THE PASSWORD
 function make_password(use){
     var new_password = '';
 
@@ -23,18 +15,8 @@ function make_password(use){
         new_password = new_password.concat(everyChar[random_num]);
     }
 
-    console.log("Password: "+new_password);
+    return new_password
 };
 
-
-//ACTUAL SCRIPT
-// var loop = 'Y';
-while (loop === 'Y'){
-    readline.question('What is this password for? ', use => {
-        make_password(use);
-        readline.question('Do you want to continue? Y/N: '), loop =>{
-            readline.close()
-        }
-        readline.close();
-    });
-}
+new_password = make_password("TY")
+console.log(new_password);
