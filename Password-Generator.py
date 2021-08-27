8155e95e9ba6c58affc5483f66158b5ff79c26cb
Veer <vtube258@gmail.com>
2021-08-27 23:26:11 +0530

    Python changes to add new passwords to a database

--- Password-Generator.py
+++ Password-Generator.py
@@ -1,4 +1,6 @@
 import random
+import sqlite3
+
 passwords = []
 
 nums = ['1','2','3','4','5','6','7','8','9','0']
@@ -8,25 +10,52 @@ symbols = ['@','#','$','%','&','.','-','_']
 
 everyChar = nums + lowercase + uppercase + symbols
 
-def makePassword():
+try:
+    conn = sqlite3.connect("Password_Database.db")
+    conn.execute("CREATE TABLE passwords (password_for text, password text)")
+    print("Making you password database")
+except:
+    print("Importing you database")
+
+
+# GENERATES THE PASSWORD
+def make_password():
 
     password = ""
-
     for i in range (0,17):
         password = password + random.choice(everyChar)
 
     return password
 
+# ADDS THE PASSWORD TO THE DATABASE
+def add_password(password):
+    print("Making a new password")
+    print(f"Password is '{password}'")
+
+    if input("Do you want to keep this password? y/n \n") == 'n':
+        pass
+        
+    else:
+        password_for = input("What is this password for? \n")
+        conn.execute(f"INSERT INTO passwords VALUES('{password_for}', '{password}')")
+
+
+# MAIN LOOP
+print("Welcome")
 while True:
-    formedPassword = makePassword()
-    print(f"Your password is '{formedPassword}'")
-    savePoint = input("What is this password for? ")
-    passwords.append({savePoint: formedPassword])
-    if input("Continue? ") == "No":
+    print("""
+
+---
+nw: To make a new password
+rt: To retrieve a password - Still under development
+qt: To quit
+---
+
+        """)
+    reason = input()
+    if reason == "nw":
+        add_password(make_password())
+        conn.commit()
+
+    if reason == "qt":
         break
-
-
-
-
-print(passwords)
-
