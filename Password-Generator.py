8e17bdfa6dba25f5c2664a2f53e9a58713ea3364
Veer <vtube258@gmail.com>
2021-08-28 12:39:49 +0530

    Polishing bugs

--- Password-Generator.py
+++ Password-Generator.py
@@ -10,17 +10,11 @@ symbols = ['@','#','$','%','&','.','-','_']
 
 everyChar = nums + lowercase + uppercase + symbols
 
-try:
-    conn = sqlite3.connect("Password_Database.db")
-    conn.execute("CREATE TABLE passwords (password_for text, password text)")
-    print("Making you password database \n")
-except:
-    print("Importing your database \n")
-    
-cursor = conn.cursor()
 
+mainPassword = "very_secret_password123" # THIS IS THE MAIN PASSWORD TO GET THE PROGRAM WORKING
 
-# GENERATES THE PASSWORD
+# GENERATING THE PASSWORD
+
 def make_password():
 
     password = ""
@@ -30,6 +24,7 @@ def make_password():
     return password
 
 # ADDS THE PASSWORD TO THE DATABASE
+
 def add_password(password):
     print("Making a new password")
     print(f"Password is '{password}'")
@@ -38,37 +33,62 @@ def add_password(password):
         pass
         
     else:
-        password_for = input("What is this password for? \n")
+        password_for = input("What is this password for? \n").lower()
         conn.execute(f"INSERT INTO passwords VALUES('{password_for}', '{password}')")
 
 
 # RETRIEVING PASSWORDS
 
 def retrieve_password():
-    retrieve_input = input("Which password do you want to retrive? ")
+    retrieveInput = input("Which password do you want to retrive? \n").lower()
     select_query = """SELECT * from passwords"""
     cursor.execute(select_query)
     records = cursor.fetchall()
 
-    print("Password is ")
     for row in records:
-        if row[0] == retrieve_input:
-            print(row[1])
+        if row[0] == retrieveInput:
+            retrieveOutput = row[1]
+
+    print(f"Password is {retrieveOutput}")
+    
 
     print("")
     print("")
 
 
 # MAIN LOOP
-print("Welcome \n")
+
+try: # MAKE THE DATABSE IF IT DOESN'T ALREADY EXIST
+    conn = sqlite3.connect("Password_Database.db")
+    conn.execute("CREATE TABLE passwords (password_for text, password text)")
+    cursor = conn.cursor()
+    print("Making your password database \n")
+
+    print("The next time you access the database, a master password will be used to make sure it is you\n")
+    print(f"'{mainPassword}' is you password, remember it well\n")
+
+except:
+    print("Importing your database \n")
+    
+
+tryPassword = input("What is you password? \n")
+
+while tryPassword != mainPassword:
+    print("Wrong password try again")
+    tryPassword = input("What is you password? \n")
+
+print("\n--- \n\nWelcome, User \n")
+
 while True:
-    print("""---
+
+    reason = input("""--- \n
+Type:
 nw: To make a new password
 rt: To retrieve a password
-qt: To quit
----
-        """)
-    reason = input()
+qt: To quit \n
+---\n
+""")
+    
     if reason == "nw":
         add_password(make_password())
         conn.commit()
